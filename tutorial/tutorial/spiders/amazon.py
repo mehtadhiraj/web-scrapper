d0697eb7260525a754ffee4caa953973542e97f6
karthikmudaliar <karthik.mudaliar16@siesgst.ac.in>
2018-09-04 23:38:10 +0530

    amazon function

--- tutorial/tutorial/spiders/amazon.py
+++ tutorial/tutorial/spiders/amazon.py
@@ -1,53 +1,45 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import pymysql
- 
+
 connection = pymysql.connect(host='localhost',
-                      user='root',
-                      password='',
-                      db='webscrapper',
-                      charset='utf8mb4',
-                      cursorclass=pymysql.cursors.DictCursor)
- 
-cursor = connection.cursor()
- 
-# Selecting.
-query = "SELECT `*` FROM `sku_master`"
-result=cursor.execute(query)
+                             user='root',
+                             password='',
+                             db='sample webscrapper',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+cursor=connection.cursor()
+
+query="Select `url` from `skuurl`"
+cursor.execute(query)
+result=cursor.fetchone()
 for res in result:
-    print (res)
-    
-
- 
-
-    
- 
-# Inserting
-#query = "INSERT INTO `table` (id) VALUES (1)"
-#cursor.execute(query)
- 
-#connection.commit() # You need this if you want your changes 'commited' to the database.
-
+    res=result['url']
+    print(res)
+    print(res[8:])
 
 class AmazonSpider(scrapy.Spider):
     name = 'amazon'
-    allowed_domains = ['www.amazon.in/Mi-Redmi-5-Black-32GB/dp/B077PWBC7J/ref=sr_1_1?s=electronics&ie=UTF8&qid=1535955509&sr=1-1&keywords=redmi+note+5.html']
-    start_urls = ['https://www.amazon.in/Mi-Redmi-5-Black-32GB/dp/B077PWBC7J/ref=sr_1_1?s=electronics&ie=UTF8&qid=1535955509&sr=1-1&keywords=redmi+note+5.html/']
+    allowed_domains = [res[8:]+'.html']
+    start_urls = [res+'.html/']
+
 
     def parse(self, response):
-       #Extract product information
-       titles = response.css('#productTitle::text').extract()
-       stock = response.css('.a-size-medium.a-color-success::text').extract()
-       prices = response.css('#priceblock_ourprice::text').extract()
-       discounts = response.css('.a-span12.a-color-price.a-size-base::text').extract()
+        #Extracting the content using css selectors
+        titles = response.css('#productTitle::text').extract()
+        price = response.css('#priceblock_dealprice::text').extract()
+        stock = response.css('.a-size-medium.a-color-success::text').extract()
+        rating = response.css('.a-icon-alt::text').extract()
+       
+        #Give the extracted content row wise
+        for item in zip(titles,price,stock,rating):
+            #create a dictionary to store the scraped info
+            scraped_info = {
+                    'title' : item[0],
+                    'price' : item[1],
+                    'stock' : item[2],
+                    'rating' : item[3],
+            }
 
-
-       for item in zip(titles,prices,stock,discounts):
-           scraped_info = {
-               'title' : item[0],
-               'price' : item[1],
-               'stock' : item[2], #Set's the url for scrapy to download images
-               'discount' : item[3]
-           }
-
-           yield scraped_info
+            #yield or give the scraped info to scrapy
+            yield scraped_info
